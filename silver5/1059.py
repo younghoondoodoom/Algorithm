5f702c443077576c7c5fc21460ec6684b217a7da
ChoiYoungHoon <dudgns2865@naver.com>
2022-04-11 10:44:25 +0900

    1059

--- silver5/1059.py
+++ silver5/1059.py
@@ -15,9 +15,16 @@ def main():
     S.sort()
     
     i = S.index(n)
-    min_num = S[i-1]
-    max_num = S[i+1]
-    return (n-min_num-1) * (max_num-n) + (max_num-n-1)
-   
+    
+    if 0 < i < L:
+        min_num = S[i-1]
+        max_num = S[i+1]
+        return (n-min_num) * (max_num-n) - 1
+    
+    elif i == 0:
+        max_num = S[1]
+        return n * (max_num-n) - 1
+    
+    
 if __name__ == '__main__':
    print(main())
