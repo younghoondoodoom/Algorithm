3df1fb89af668ec3933d4e15eae07bc804576eec
ChoiYoungHoon <dudgns2865@naver.com>
2022-04-18 22:18:09 +0900

    1380

---
+++ silver5/1380.py
@@ -0,0 +1,23 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def main():
+    case = 1
+    while 1:
+        n = int(input())
+        if n == 0:
+            break
+        names = [input() for _ in range(n)]
+        li = [0]*n
+        for _ in range(2*n-1):
+            i, a = input().split()
+            li[int(i)-1] += 1
+        res = names[[i for i in range(n) if li[i] != 2][0]]
+        print(f"{case} {res}")
+        case += 1
+        
+        
+if __name__ == '__main__':
+    main()
