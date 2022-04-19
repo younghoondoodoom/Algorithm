6d52fe6ab08723ef713528ccaec7d973ddfc5a39
ChoiYoungHoon <dudgns2865@naver.com>
2022-04-19 22:41:21 +0900

    1064

--- silver5/1064.py
+++ silver5/1064.py
@@ -4,10 +4,29 @@ def intput():
     return sys.stdin.readline().rstrip()
 
 def main():
-    x_a, y_a, x_b, y_b, x_c, y_c = list(map(int, input().split(' ')))
+    x_a, y_a, x_b, y_b, x_c, y_c = map(int, input().split(' '))
+    try:
+        i = (x_a - x_b) / (y_a - y_b)
+    except ZeroDivisionError:
+        i = 0
+    try:
+        j = (x_a - x_c) / (y_a - y_c)
+    except ZeroDivisionError:
+        j = 0
+    try:
+        k = (x_b - x_c) / (y_b - y_c)
+    except ZeroDivisionError:
+        k = 0
+    
+    if i == j == k:
+        return -1.0
+    
+    lis = [((x_a - x_b)**2 + (y_a - y_b)**2)**0.5, ((x_a - x_c)**2 + (y_a - y_c)**2)**0.5, ((x_b - x_c)**2 + (y_b - y_c)**2)**0.5]
+    lis.sort()
+    return(abs(2*(lis[0] - lis[2])))
     
     
     
 
 if __name__ == '__main__':
-    main()
+    print(main())
